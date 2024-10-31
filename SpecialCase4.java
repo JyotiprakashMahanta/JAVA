7ca3aa5ad898eb2895a5d538b5e8c3eed8fa2877
Jyotiprakash Mahanta <160344458+JyotiprakashMahanta@users.noreply.github.com>
2024-10-31 10:42:21 +0530

    Add files via upload

---
+++ SpecialCase4.java
@@ -0,0 +1,11 @@
+package toString;
+
+public class SpecialCase4 {
+
+	public static void main(String[] args) {
+		String s="Jyotiprakash";
+		int n=5;
+		System.out.println(s.substring(n)+s.substring(0,n));
+	}
+
+}
