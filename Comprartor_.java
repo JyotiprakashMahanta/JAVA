e1e250edef707e723959710a7853689bb8ed5d51
Jyotiprakash Mahanta <160344458+JyotiprakashMahanta@users.noreply.github.com>
2024-11-15 10:29:45 +0530

    Add files via upload

---
+++ Comprartor_.java
@@ -0,0 +1,34 @@
+package Comprartor;
+
+
+/**
+ * COMPARATOR
+ * ===========
+ * It is an Interface which is present in  java.util package.
+ * Which is consist a abstract() method called compare which help the developer to perform dynamic sorting
+ * i.e user define sorting.
+ * 
+ *  syntax:
+ *  ---------
+ *  public int compare (Object o1,Object o2){
+ *  	return 1;
+ *  }
+ *  
+ *  To perform user defined sorting we should create helper class for each sorting option.
+ *  
+ *   Every helper classes must be implemented comprartor Interface and should override with compare() 
+ *   with following case.
+ *   
+ *   case-1
+ *   Object 1 property> Object 2 property (it return positive integer).
+ *   case-2
+ *   Object 1 property < Object 2 property (it return negative integer).
+ *   case-3
+ *   Object 1 property == Object 2 property then it returns zero as a integer.
+ * 
+ * 
+ * 
+ * */
+public class Comprartor_ {
+
+}
